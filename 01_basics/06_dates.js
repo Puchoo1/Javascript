4c6cc2a144b09e7ee5365f29914a620378fc5b1b
Puchoo1 <158981027+Puchoo1@users.noreply.github.com>
2024-02-05 06:29:36 +0000

    dates_js

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,19 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleDateString);
+
+
+const myBirthdate = new Date(2000, 1 , 4);
+console.log(myBirthdate.toDateString());
+
+
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp) ;
+// console.log(myBirthdate.getTime()) ;
