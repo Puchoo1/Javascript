1f2c81c8725acf0af24dc51b8cca6493946b30a3
Puchoo1 <158981027+Puchoo1@users.noreply.github.com>
2024-02-05 05:20:08 +0000

    strings

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,11 @@
+const name = "Rahul";
+const WPM = 50;
+
+// console.log(name + WPM + "Value");// AAm jindgi
+
+console.log(`Hello my name is ${name} and my typing speed is ${WPM}`);
+const gameName = new String("Rahul kumar")
+console.log(gameName.__proto__);
+
+const newString = gameName.slice(0, 4);
+console.log(newString);
