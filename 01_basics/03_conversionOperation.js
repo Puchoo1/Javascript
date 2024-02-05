acbb22acc3d49534d6fc2669a41558d465ab96ae
Puchoo1 <158981027+Puchoo1@users.noreply.github.com>
2024-02-05 04:08:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let age = "67abx";
+let empty ="";
+
+console.log(typeof age);
+console.log(typeof (age));
+
+// conversion
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+// *Note* : when we change string to number remember it also can be changed as NaN not a number;
+console.log(valueInNumber);
+let Newempty= Boolean(empty);
+console.log(Newempty);
+
+// When we convert 
+// empty"" => false
+// "string" => true
