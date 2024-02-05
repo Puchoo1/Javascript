773b526a2d4d7bfe1b7c61295741b3863906c504
Puchoo1 <158981027+Puchoo1@users.noreply.github.com>
2024-02-05 03:45:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // It treats all JS code as newer version
+
+// alert("Hello") // it is used on browser
+
+// Datatypes are of 2 types Primitive and Non Primitive
+
+// Primitive
+let name = 'Rahul';     // 1.string
+let age = 23;           //  2.number
+let goodPerson = false; // 3. bolean
+// bigInt
+//null =>  standalone value(its empty value)
+//undefined
+//symbol => use in react (unique)
+
+
+
+//  object
+
+console.log(typeof"Rahul");
+
+// None primitive datatypes...!
+
+// objects
+// array
+// strings
